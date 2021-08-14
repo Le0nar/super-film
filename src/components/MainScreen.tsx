1e838fceae37664a4692c4c3bf1898f084803ed6
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-08-14 15:44:22 +0400

    feat: add Calendar component

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,5 +1,14 @@
 import { FC } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { useHistory } from "react-router-dom";
 
 export const MainScreen: FC = () => {
-  return <div>MainScreen</div>;
+  const history = useHistory();
+
+  const goToSchedule = () => {
+    history.push("/schedule");
+  };
+
+  return <Calendar onClickDay={goToSchedule} />;
 };
